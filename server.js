446110eeacff8cea893ccf08621a70838e198d5e
jucinaldo.duarte <jucinaldo.duarte@gmail.com>
2024-01-07 15:57:21 -0300

    modified:   package-lock.json modified:   package.json modified:   server.js

--- server.js
+++ server.js
@@ -10,11 +10,12 @@ var port = process.env.PORT || 3000;
 
 const express = require('express');
 const app = express();
+const port = 3000;
  
 app.get('/', (req, res) => {
   res.send("Hello");
 });
  
-app.listen(process.env.PORT || 3000, () => {
-  console.log('Web Server is listening at port ' + (process.env.PORT || 3000));
+app.listen(process.env.PORT || port, () => {
+  console.log('Web Server is listening at port ' + (process.env.PORT || port));
 });
