9837b51d1286e2808f34231739417793486794f3
jucinaldo.duarte <jucinaldo.duarte@gmail.com>
2024-01-11 18:02:52 -0300

    renamed:    W01_Individual_Activity/.gitignore -> .gitignore deleted:    W01_Individual_Activity/package.json deleted:    W01_Learning_Activity/.gitignore deleted:    W01_Learning_Activity/package-lock.json deleted:    W01_Learning_Activity/server.js renamed:    W01_Individual_Activity/package-lock.json -> package-lock.json renamed:    W01_Learning_Activity/package.json -> package.json renamed:    W01_Individual_Activity/server.js -> server.js

--- server.js
+++ server.js
@@ -1,5 +1,7 @@
+// express web server
 const express = require('express');
 const app = express();
+const port = process.env.PORT || 3000;
  
 app.get('/', (req, res) => {
   res.send("Hello");
@@ -8,3 +10,6 @@ app.get('/', (req, res) => {
 app.listen(process.env.PORT || 3000, () => {
   console.log('Web Server is listening at port ' + (process.env.PORT || 3000));
 });
+
+
+
