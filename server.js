3fd544024546a45ffe8f25a9d427e6acfd084626
jucinaldo.duarte <jucinaldo.duarte@gmail.com>
2024-01-12 20:44:28 -0300

    modified:   server.js

--- server.js
+++ server.js
@@ -1,15 +1,20 @@
 // express web server
 const express = require('express');
 const app = express();
-const port = process.env.PORT || 3000;
- 
+const port = 3000;
+
 app.get('/', (req, res) => {
-  res.send("Hello");
-});
- 
-app.listen(process.env.PORT || 3000, () => {
-  console.log('Web Server is listening at port ' + (process.env.PORT || 3000));
+    res.send("Root");
 });
 
+app.get('/home', (req, res) => {
+    res.send("Home");
+});
 
-
+app.get('/Contact', (req, res) => {
+    res.send("Contact");
+});
+   
+  app.listen(process.env.PORT || port, () => {
+    console.log('Web Server is listening at port ' + (process.env.PORT || port));
+});
